2bf14de93620fc7f46fa256b729ec9ba477426cc
milenycgalvao <mileny.cristine1@gmail.com>
2021-09-20 18:50:05 -0300

    adicionado list view que renderiza tudo

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -14,49 +14,52 @@ class TransactionList extends StatefulWidget {
 class _TransactionListState extends State<TransactionList> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: widget.transactions.map((tr) {
-        return Card(
-            child: Row(
-          children: [
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-              decoration: BoxDecoration(
-                  border: Border.all(
-                color: Colors.red.shade800,
-                width: 2,
-              )),
-              padding: EdgeInsets.all(10),
-              child: Text(
-                'R\$ ${tr.value.toStringAsFixed(2)}',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
+    return Container(
+      height: 500,
+      child: ListView(
+        children: widget.transactions.map((tr) {
+          return Card(
+              child: Row(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                decoration: BoxDecoration(
+                    border: Border.all(
                   color: Colors.red.shade800,
+                  width: 2,
+                )),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  'R\$ ${tr.value.toStringAsFixed(2)}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.red.shade800,
+                  ),
                 ),
               ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  tr.title,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    tr.title,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                Text(
-                  DateFormat('d MMM y').format(tr.date),
-                  style: TextStyle(
-                    color: Colors.grey,
+                  Text(
+                    DateFormat('d MMM y').format(tr.date),
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
                   ),
-                ),
-              ],
-            )
-          ],
-        ));
-      }).toList(),
+                ],
+              )
+            ],
+          ));
+        }).toList(),
+      ),
     );
   }
 }
